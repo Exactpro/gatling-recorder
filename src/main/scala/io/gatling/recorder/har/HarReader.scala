gatling/gatling

Unit test the HAR converter and rewrite in  fonctionnal style for easier testing

gatling-recorder/src/main/scala/io/gatling/recorder/har/HarReader.scala
@@ -15,94 +15,76 @@
  */
 package io.gatling.recorder.har
 
+import java.io.{ FileInputStream, InputStream }
 import java.net.URL
 
-import scala.concurrent.duration.DurationLong
-import scala.io.Source
+import scala.concurrent.duration._
 import scala.util.Try
 
-import org.joda.convert.StringConvert
-import org.joda.time.DateTime
-
-import io.gatling.core.config.GatlingConfiguration.configuration
+import io.gatling.recorder.config.RecorderConfiguration.{ configuration => recorderConfiguration }
+import io.gatling.core.util.IOHelper
 import io.gatling.core.util.StringHelper.RichString
 import io.gatling.http.HeaderNames.CONTENT_TYPE
-import io.gatling.recorder.config.RecorderConfiguration.{ configuration => recorderConfiguration }
 import io.gatling.recorder.scenario.{ PauseElement, RequestBodyBytes, RequestBodyParams, RequestElement, ScenarioElement }
+import io.gatling.recorder.util.Json
 import io.gatling.recorder.util.RedirectHelper.isRequestRedirect
 
 object HarReader {
 
-	var scenarioElements: List[ScenarioElement] = Nil
+	def apply(path: String): List[ScenarioElement] =
+		IOHelper.withCloseable(new FileInputStream(path))(apply(_))
 
-	private var lastEntry: Entry = _
-	private var lastRequestTimestamp: Long = 0
+	def apply(jsonStream: InputStream): List[ScenarioElement] =
+		apply(Json.parseJson(jsonStream))
 
-	def processHarFile(path: String) {
-		def isValidURL(url: String) = Try(new URL(url)).isSuccess
-
-		val json = Source.fromFile(path).getLines.mkString
-		val httpArchive = HarMapping.jsonToHttpArchive(json)
-		httpArchive.log.entries.filter(entry => isValidURL(entry.request.url)).foreach(processEntry)
-
+	def apply(json: Json): List[ScenarioElement] = {
+		val HttpArchive(Log(entries)) = HarMapping.jsonToHttpArchive(json)
+		entries.iterator
+			.filter(e => isValidURL(e.request.url))
+			// MOVE THIS OUT - we want a sequence of filters ... 
+			// the fetch resources need to be applied here too.
+			.filter(e => recorderConfiguration.filters.filters.map(_.accept(e.request.url)).getOrElse(true))
+			.flatMap(createScenarioElements(_))
+			.toList
 	}
 
-	def cleanHarReaderState {
-		scenarioElements = Nil
-		lastEntry = null
-		lastRequestTimestamp = 0
-	}
-
-	private def processEntry(entry: Entry) {
-
-		def buildHeaders(entry: Entry) = {
-			val headers = entry.request.headers.map(header => (header.name, header.value)).toMap
-			// NetExport doesn't add Content-Type to headers when POSTing, but both Chrome Dev Tools and NetExport set mimeType
-			entry.request.postData.map(postData => headers.updated(CONTENT_TYPE, postData.mimeType)).getOrElse(headers)
-		}
-
-		def createRequest(entry: Entry, statusCode: Int) {
-			def buildContent(postParams: Seq[PostParam]) = RequestBodyParams(postParams.map(postParam => (postParam.name, postParam.value)).toList)
-
-			val uri = entry.request.url
-			val method = entry.request.method
-			val headers = buildHeaders(entry)
-			// NetExport doesn't copy post params to text field
-			val body = entry.request.postData.map { postData =>
-				postData.text.trimToOption match {
-					case Some(string) => RequestBodyBytes(string.getBytes(configuration.core.encoding))
-					case None => buildContent(postData.params)
-				}
-			}
-			scenarioElements = RequestElement(uri, method, headers, body, statusCode, None) :: scenarioElements
-		}
-
-		def createPause {
-			def parseMillisFromIso8601DateTime(time: String) = StringConvert.INSTANCE.convertFromString(classOf[DateTime], time).getMillis
-
-			val timestamp = parseMillisFromIso8601DateTime(entry.startedDateTime)
-			val diff = timestamp - lastRequestTimestamp
-			if (lastRequestTimestamp != 0 && diff > 10) {
-				scenarioElements = new PauseElement(diff milliseconds) :: scenarioElements
-			}
-			lastRequestTimestamp = timestamp
-		}
-
-		if (recorderConfiguration.filters.filters.map(_.accept(entry.request.url)).getOrElse(true)) {
-			if (lastEntry == null && isRequestRedirect(entry.response.status)) {
-				createPause
-				lastEntry = entry
-
-			} else if (lastEntry != null && !isRequestRedirect(entry.response.status)) {
-				// process request with new status
-				createRequest(lastEntry, entry.response.status)
-				lastEntry = null
-
-			} else if (lastEntry == null) {
-				// standard use case
-				createPause
-				createRequest(entry, entry.response.status)
-			}
+	private def createScenarioElements(entry: Entry): List[ScenarioElement] = {
+		if (isRequestRedirect(entry.response.status)) {
+			List()
+		} else {
+			val req = createRequest(entry)
+			// TODO we should have a configuration for that.
+			if (entry.lag <= 10) {
+				List(req)
+			} else
+				List(new PauseElement(entry.lag milliseconds), req)
 		}
 	}
+
+	private def createRequest(entry: Entry): RequestElement = {
+		def buildContent(postParams: Seq[PostParam]) =
+			RequestBodyParams(postParams.map(postParam => (postParam.name, postParam.value)).toList)
+
+		val uri = entry.request.url
+		val method = entry.request.method
+		val headers = buildHeaders(entry)
+
+		// NetExport doesn't copy post params to text field
+		val body = entry.request.postData.map { postData =>
+			postData.text.trimToOption match {
+				case Some(string) => RequestBodyBytes(string.getBytes(recorderConfiguration.core.encoding))
+				case None => buildContent(postData.params)
+			}
+		}
+
+		RequestElement(uri, method, headers, body, entry.response.status, None)
+	}
+
+	private def buildHeaders(entry: Entry): Map[String, String] = {
+		val headers = entry.request.headers.map(h => (h.name, h.value)).toMap
+		// NetExport doesn't add Content-Type to headers when POSTing, but both Chrome Dev Tools and NetExport set mimeType
+		entry.request.postData.map(postData => headers.updated(CONTENT_TYPE, postData.mimeType)).getOrElse(headers)
+	}
+
+	private def isValidURL(url: String): Boolean = Try(new URL(url)).isSuccess
 }

gatling-recorder/src/test/scala/io/gatling/recorder/har/HarReaderSpec.scala
@@ -1,0 +1,74 @@
+/**
+ * Copyright 2011-2013 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.recorder.har
+
+import scala.collection.mutable
+import scala.concurrent.duration._
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import io.gatling.core.util.IOHelper
+import io.gatling.recorder.config.RecorderConfiguration
+import io.gatling.recorder.util.Json.parseJson
+import io.gatling.recorder.scenario.PauseElement
+import org.specs2.matcher.BeLessThan
+import io.gatling.recorder.scenario.RequestElement
+
+@RunWith(classOf[JUnitRunner])
+class HarReaderSpec extends Specification {
+
+	def resourceAsStream(p: String) = getClass.getClassLoader.getResourceAsStream(p)
+	val harEmptyJson = IOHelper.withCloseable(resourceAsStream("har/empty.har"))(parseJson(_))
+	val harKernelJson = IOHelper.withCloseable(resourceAsStream("har/www.kernel.org.har"))(parseJson(_))
+
+	RecorderConfiguration.initialSetup(mutable.HashMap.empty, None)
+
+	"HarReader" should {
+
+		"work with empty JSON" in {
+			HarReader(harEmptyJson) must beEmpty
+		}
+
+		val elts = HarReader(harKernelJson)
+		val pauseElts = elts.collect { case PauseElement(duration) => duration }
+
+		"return the correct number of Pause elements" in {
+			pauseElts.size must beLessThan(elts.size / 2)
+		}
+
+		"return an appropriate pause duration" in {
+			val pauseDuration = pauseElts.foldLeft(0 milliseconds)(_ + _)
+
+			// The total duration of the HAR record is of 6454ms
+			(pauseDuration must beLessThanOrEqualTo(88389 milliseconds)) and
+				(pauseDuration must beGreaterThan(80000 milliseconds))
+		}
+
+		"return the right request elements" in {
+			val (googleFontUris, uris) = elts
+				.collect { case RequestElement(uri, _, _, _, _, _) => uri }
+				.partition(_.contains("google"))
+
+			(uris must contain(startingWith("https://www.kernel.org")).forall) and
+				(uris.size must beEqualTo(41)) and
+				(googleFontUris.size must beEqualTo(16)) and
+				(uris.head must beEqualTo("https://www.kernel.org/"))
+		}
+	}
+
+	// Deactivate Specs2 implicit to be able to use the ones provided in scala.concurrent.duration
+	override def intToRichLong(v: Int) = super.intToRichLong(v)
+}

Exactpro/gatling-recorder

src/main/scala/io/gatling/recorder/har/HarReader.scala
@@ -15,93 +15,76 @@
  */
 package io.gatling.recorder.har
 
+import java.io.{FileInputStream, InputStream}
 import java.net.URL
 
 import scala.concurrent.duration.DurationLong
-import scala.io.Source
 import scala.util.Try
 
-import org.joda.convert.StringConvert
-import org.joda.time.DateTime
-
+import io.gatling.core.util.IOHelper
 import io.gatling.core.util.StringHelper.RichString
 import io.gatling.http.HeaderNames.CONTENT_TYPE
 import io.gatling.recorder.config.RecorderConfiguration.configuration
-import io.gatling.recorder.scenario.{ PauseElement, RequestBodyBytes, RequestBodyParams, RequestElement, ScenarioElement }
+import io.gatling.recorder.scenario.{PauseElement, RequestBodyBytes, RequestBodyParams, RequestElement, ScenarioElement}
+import io.gatling.recorder.util.Json
 import io.gatling.recorder.util.RedirectHelper.isRequestRedirect
 
 object HarReader {
 
-	var scenarioElements: List[ScenarioElement] = Nil
+	def apply(path: String): List[ScenarioElement] =
+		IOHelper.withCloseable(new FileInputStream(path))(apply(_))
 
-	private var lastEntry: Entry = _
-	private var lastRequestTimestamp: Long = 0
+	def apply(jsonStream: InputStream): List[ScenarioElement] =
+		apply(Json.parseJson(jsonStream))
 
-	def processHarFile(path: String) {
-		def isValidURL(url: String) = Try(new URL(url)).isSuccess
-
-		val json = Source.fromFile(path).getLines.mkString
-		val httpArchive = HarMapping.jsonToHttpArchive(json)
-		httpArchive.log.entries.filter(entry => isValidURL(entry.request.url)).foreach(processEntry)
-
+	def apply(json: Json): List[ScenarioElement] = {
+		val HttpArchive(Log(entries)) = HarMapping.jsonToHttpArchive(json)
+		entries.iterator
+			.filter(e => isValidURL(e.request.url))
+			// MOVE THIS OUT - we want a sequence of filters ... 
+			// the fetch resources need to be applied here too.
+			.filter(e => configuration.filters.filters.map(_.accept(e.request.url)).getOrElse(true))
+			.flatMap(createScenarioElements(_))
+			.toList
 	}
 
-	def cleanHarReaderState {
-		scenarioElements = Nil
-		lastEntry = null
-		lastRequestTimestamp = 0
-	}
-
-	private def processEntry(entry: Entry) {
-
-		def buildHeaders(entry: Entry) = {
-			val headers = entry.request.headers.map(header => (header.name, header.value)).toMap
-			// NetExport doesn't add Content-Type to headers when POSTing, but both Chrome Dev Tools and NetExport set mimeType
-			entry.request.postData.map(postData => headers.updated(CONTENT_TYPE, postData.mimeType)).getOrElse(headers)
-		}
-
-		def createRequest(entry: Entry, statusCode: Int) {
-			def buildContent(postParams: Seq[PostParam]) = RequestBodyParams(postParams.map(postParam => (postParam.name, postParam.value)).toList)
-
-			val uri = entry.request.url
-			val method = entry.request.method
-			val headers = buildHeaders(entry)
-			// NetExport doesn't copy post params to text field
-			val body = entry.request.postData.map { postData =>
-				postData.text.trimToOption match {
-					case Some(string) => RequestBodyBytes(string.getBytes(configuration.core.encoding))
-					case None => buildContent(postData.params)
-				}
-			}
-			scenarioElements = RequestElement(uri, method, headers, body, statusCode, None) :: scenarioElements
-		}
-
-		def createPause {
-			def parseMillisFromIso8601DateTime(time: String) = StringConvert.INSTANCE.convertFromString(classOf[DateTime], time).getMillis
-
-			val timestamp = parseMillisFromIso8601DateTime(entry.startedDateTime)
-			val diff = timestamp - lastRequestTimestamp
-			if (lastRequestTimestamp != 0 && diff > 10) {
-				scenarioElements = new PauseElement(diff milliseconds) :: scenarioElements
-			}
-			lastRequestTimestamp = timestamp
-		}
-
-		if (configuration.filters.filters.map(_.accept(entry.request.url)).getOrElse(true)) {
-			if (lastEntry == null && isRequestRedirect(entry.response.status)) {
-				createPause
-				lastEntry = entry
-
-			} else if (lastEntry != null && !isRequestRedirect(entry.response.status)) {
-				// process request with new status
-				createRequest(lastEntry, entry.response.status)
-				lastEntry = null
-
-			} else if (lastEntry == null) {
-				// standard use case
-				createPause
-				createRequest(entry, entry.response.status)
-			}
+	private def createScenarioElements(entry: Entry): List[ScenarioElement] = {
+		if (isRequestRedirect(entry.response.status)) {
+			List()
+		} else {
+			val req = createRequest(entry)
+			// TODO we should have a configuration for that.
+			if (entry.lag <= 10) {
+				List(req)
+			} else
+				List(new PauseElement(entry.lag milliseconds), req)
 		}
 	}
+
+	private def createRequest(entry: Entry): RequestElement = {
+		def buildContent(postParams: Seq[PostParam]) =
+			RequestBodyParams(postParams.map(postParam => (postParam.name, postParam.value)).toList)
+
+		val uri = entry.request.url
+		val method = entry.request.method
+		val headers = buildHeaders(entry)
+
+		// NetExport doesn't copy post params to text field
+		val body = entry.request.postData.map { postData =>
+			postData.text.trimToOption match {
+				case Some(string) => RequestBodyBytes(string.getBytes(configuration.core.encoding))
+				case None => buildContent(postData.params)
+			}
+		}
+
+		RequestElement(uri, method, headers, body, entry.response.status, None)
+	}
+
+	private def buildHeaders(entry: Entry): Map[String, String] = {
+		val headers = entry.request.headers.map(h => (h.name, h.value)).toMap
+		// NetExport doesn't add Content-Type to headers when POSTing, but both Chrome Dev Tools and NetExport set mimeType
+		entry.request.postData.map(postData => headers.updated(CONTENT_TYPE, postData.mimeType)).getOrElse(headers)
+	}
+
+	private def isValidURL(url: String): Boolean = Try(new URL(url)).isSuccess
 }

src/test/scala/io/gatling/recorder/har/HarReaderSpec.scala
@@ -1,0 +1,72 @@
+/**
+ * Copyright 2011-2013 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.recorder.har
+
+import scala.collection.mutable
+import scala.concurrent.duration.DurationInt
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import io.gatling.core.util.IOHelper
+import io.gatling.recorder.config.RecorderConfiguration
+import io.gatling.recorder.scenario.{PauseElement, RequestElement}
+import io.gatling.recorder.util.Json.parseJson
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class HarReaderSpec extends Specification {
+
+	def resourceAsStream(p: String) = getClass.getClassLoader.getResourceAsStream(p)
+	val harEmptyJson = IOHelper.withCloseable(resourceAsStream("har/empty.har"))(parseJson(_))
+	val harKernelJson = IOHelper.withCloseable(resourceAsStream("har/www.kernel.org.har"))(parseJson(_))
+
+	RecorderConfiguration.initialSetup(mutable.HashMap.empty, None)
+
+	"HarReader" should {
+
+		"work with empty JSON" in {
+			HarReader(harEmptyJson) must beEmpty
+		}
+
+		val elts = HarReader(harKernelJson)
+		val pauseElts = elts.collect { case PauseElement(duration) => duration }
+
+		"return the correct number of Pause elements" in {
+			pauseElts.size must beLessThan(elts.size / 2)
+		}
+
+		"return an appropriate pause duration" in {
+			val pauseDuration = pauseElts.foldLeft(0 milliseconds)(_ + _)
+
+			// The total duration of the HAR record is of 6454ms
+			(pauseDuration must beLessThanOrEqualTo(88389 milliseconds)) and
+				(pauseDuration must beGreaterThan(80000 milliseconds))
+		}
+
+		"return the right request elements" in {
+			val (googleFontUris, uris) = elts
+				.collect { case RequestElement(uri, _, _, _, _, _) => uri }
+				.partition(_.contains("google"))
+
+			(uris must contain(startingWith("https://www.kernel.org")).forall) and
+				(uris.size must beEqualTo(41)) and
+				(googleFontUris.size must beEqualTo(16)) and
+				(uris.head must beEqualTo("https://www.kernel.org/"))
+		}
+	}
+
+	// Deactivate Specs2 implicit to be able to use the ones provided in scala.concurrent.duration
+	override def intToRichLong(v: Int) = super.intToRichLong(v)
+}